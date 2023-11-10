4017e4abbe184ee7e13ad0b03209162e5bbe8de4
bw <bartwol111@gmail.com>
2023-11-10 14:54:15 +0100

    setup

---
+++ Tests/IntegrationTests/ControllerIntegrationTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Tests.IntegrationTests
+{
+    internal class ControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        protected readonly HttpClient _HttpClient;
+        protected readonly DataContext _context;
+        protected readonly IConfiguration _configuration;
+
+        public ControllerIntegrationTests(CustomWebApplicationFactory<Program> fixture)
+        {
+            _HttpClient = fixture._httpClient;
+            _context = fixture._context;
+            _configuration = fixture._configuration;
+
+            string adminTestToken = _configuration["AdminTestToken"];
+            _HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminTestToken);
+        }
+
+        protected async Task<T> DeserializeResponse<T>(HttpResponseMessage response) where T : class
+        {
+            var resp = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(resp);
+        }
+
+        protected async Task AddToDatabase(IEnumerable<IDbModel> models)
+        {
+            await _context.AddRangeAsync(models);
+            await _context.SaveChangesAsync();
+        }
+
+        protected async Task<IDbModel> AddToDatabase(IDbModel model)
+        {
+            var addedModel = await _context.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return addedModel.Entity;
+        }
+
+        protected void AssertResponseSuccess(ServiceResponse responseContent)
+        {
+            Assert.True(responseContent.SuccessStatus, responseContent.Message);
+        }
+
+        protected void AssertResponseFailure(ServiceResponse responseContent)
+        {
+            Assert.False(responseContent.SuccessStatus, responseContent.Message);
+        }
+
+        protected virtual async Task<List<T>> FindInDatabaseByConditions<T>(Expression<Func<T, bool>> expresion) where T : class
+        {
+            return await _context.Set<T>().Where(expresion).ToListAsync();
+        }
+
+        protected virtual async Task<T> FindInDatabaseByConditionsFirstOrDefault<T>(Expression<Func<T, bool>> expresion) where T : class
+        {
+            return await _context.Set<T>().Where(expresion).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ Tests/TestServices/TestApiUserService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests.TestServices
+{
+    internal class TestAppUserService : IAppUserService
+    {
+        public int GetCurrentUserId()
+        {
+            return 1;
+        }
+
+        public bool CurrentUserIsAdmin()
+        {
+            return false;
+        }
+    }
+}
