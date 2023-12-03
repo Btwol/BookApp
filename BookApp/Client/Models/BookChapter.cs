547b62ffe75e1c3bbe8d0a94e050b2165b2d318f
bw <bartwol111@gmail.com>
2023-12-03 15:07:52 +0100

    moved SummaryHighlightComponent

---
+++ BookApp/Client/Models/BookChapter.cs
@@ -0,0 +1,8 @@
+﻿namespace BookApp.Client.Models
+{
+    public class BookChapter
+    {
+        public int ChapterNumber { get; set; }
+        public Dictionary<int,string> Paragraphs { get; set; }  //textNode, textContent
+    }
+}
