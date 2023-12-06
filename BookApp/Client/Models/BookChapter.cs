da4649acffca9e1f82d77ced1562df2dfa912fa8
bw <bartwol111@gmail.com>
2023-12-06 16:39:33 +0100

    finished summary view

--- BookApp/Client/Models/BookChapter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookApp.Client.Models
-{
-    public class BookChapter
-    {
-        public int ChapterNumber { get; set; }
-        public Dictionary<int,string> Paragraphs { get; set; }  //textNode, textContent
-    }
-}
--- BookApp/Client/Models/ReaderPosition.cs
+++ BookApp/Client/Models/ReaderPosition.cs
@@ -4,6 +4,7 @@
     {
         public double ScrollPosition { get; set; } = 0;
         public int Page { get; set; } = 0;
+        public int? TextNode { get; set; }
         public string ElementIdToScrollTo { get; set; } = "";
     }
 }
