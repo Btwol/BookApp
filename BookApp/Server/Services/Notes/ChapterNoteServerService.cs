d8a04ea0939cab4aba0b2bbb9d544bbf606d94ec
bw <bartwol111@gmail.com>
2023-12-10 15:44:10 +0100

    new notes have filtered tags

--- BookApp/Server/Services/Notes/ChapterNoteServerService.cs
+++ BookApp/Server/Services/Notes/ChapterNoteServerService.cs
@@ -3,8 +3,8 @@
     public class ChapterNoteServerService : NoteServerService<ChapterNote, ChapterNoteModel>, IChapterNoteServerService
     {
         public ChapterNoteServerService(IChapterNoteMapper noteMapper, IBookAnalysisRepository bookAnalysisRepository,
-            IChapterNoteRepository noteRepository, IBookAnalysisServerService bookAnalysisServerService, IHubServerService hubServerService)
-            : base(noteMapper, bookAnalysisRepository, noteRepository, bookAnalysisServerService, hubServerService)
+            IChapterNoteRepository noteRepository, IBookAnalysisServerService bookAnalysisServerService, IHubServerService hubServerService, ITagRepository tagRepository)
+            : base(noteMapper, bookAnalysisRepository, noteRepository, bookAnalysisServerService, hubServerService, tagRepository)
         {
         }
     }
