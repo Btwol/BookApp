8f684800d2b5e36d689f3f0bd78baba77fa94916
bw <bartwol111@gmail.com>
2023-12-12 15:11:49 +0100

    optimized book and analysis loading

--- BookApp/Server/Services/MapperServices/BookAnalysisMapper.cs
+++ BookApp/Server/Services/MapperServices/BookAnalysisMapper.cs
@@ -6,11 +6,13 @@ namespace BookApp.Server.Services.MapperServices
     {
         private readonly IBookAnalysisUserRepository _bookAnalysisUserRepository;
         private readonly IAppUserMapperService _appUserMapperService;
+        private readonly ITagMapper _tagMapper;
 
-        public BookAnalysisMapper(IMapper mapper, IBookAnalysisUserRepository bookAnalysisUserRepository, IAppUserMapperService appUserMapperService) : base(mapper)
+        public BookAnalysisMapper(IMapper mapper, IBookAnalysisUserRepository bookAnalysisUserRepository, IAppUserMapperService appUserMapperService, ITagMapper tagMapper) : base(mapper)
         {
             _bookAnalysisUserRepository = bookAnalysisUserRepository;
             _appUserMapperService = appUserMapperService;
+            _tagMapper = tagMapper;
         }
 
         public void MapEditBookAnalysis(BookAnalysis analysistoUpdate, BookAnalysisSummaryModel updatedBookAnalysisModel)
@@ -22,6 +24,19 @@ namespace BookApp.Server.Services.MapperServices
         {
             var mappedAnalysis = _mapper.Map<BookAnalysisDetailedModel>(bookAnalysis);
             await IncludeMembers(bookAnalysis, mappedAnalysis);
+            foreach(var tag in bookAnalysis.Tags)
+            {
+                foreach (var analysisNote in tag.AnalysisNotes) mappedAnalysis.AnalysisNotes.FirstOrDefault(a => a.Id == analysisNote.Id).Tags.Add(await _tagMapper.MapToClientModel(tag));
+                foreach (var chapterNote in tag.ChapterNotes) mappedAnalysis.ChapterNotes.FirstOrDefault(a => a.Id == chapterNote.Id).Tags.Add(await _tagMapper.MapToClientModel(tag));
+                foreach (var paragraphNote in tag.ParagraphNotes) mappedAnalysis.ParagraphNotes.FirstOrDefault(a => a.Id == paragraphNote.Id).Tags.Add(await _tagMapper.MapToClientModel(tag));
+                foreach (var highlight in tag.Highlights)
+                {
+                    mappedAnalysis.Highlights.FirstOrDefault(a => a.Id == highlight.Id).Tags.Add(await _tagMapper.MapToClientModel(tag));
+
+                    foreach (var highlightNote in tag.HighlightNotes) mappedAnalysis.Highlights.FirstOrDefault(h => h.Id == highlight.Id)
+                            .HighlightNotes.FirstOrDefault(a => a.Id == highlightNote.Id).Tags.Add(await _tagMapper.MapToClientModel(tag));
+                }
+            }
             return mappedAnalysis;
         }
 
